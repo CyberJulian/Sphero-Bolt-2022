3e1e5cc06c4c0e78b5fdf274bcfe63955fca6080
Will <cyberwjulian@gmail.com>
2022-01-26 12:36:22 -0500

    initial Haunted Mansion Commit

---
+++ Haunted Mansion/Haunted Mansion Program/HauntedMansion.js
@@ -0,0 +1,7 @@
+/*
+Programmer: William Julian
+Team Member: Becket Jacobs
+Date: 1.26.2022
+Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted
+Sounds, Changing of colours, and other obstacles along the way using a Sphero Bolt and JavaScript
+*/
