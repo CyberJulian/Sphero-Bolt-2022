32c322900021267a8ffb27d68e461beffeb21a72
Will <cyberwjulian@gmail.com>
2022-01-31 12:02:18 -0500

    Blue light complete - William

--- Haunted Mansion/Haunted Mansion Program/HauntedMansion.js
+++ Haunted Mansion/Haunted Mansion Program/HauntedMansion.js
@@ -5,3 +5,10 @@ Date: 1.26.2022
 Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted
 Sounds, Changing of colours, and other obstacles along the way using a Sphero Bolt and JavaScript
 */
+
+async function startProgram() {
+	await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true);
+	setMainLed({ r: 90, g: 70, b: 0})
+	await roll(0, 100, 1.3)
+	setMainLed({ r: 0, g: 0, b: 255})
+}
